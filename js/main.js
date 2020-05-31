0e6aab902ab11aa5f9ad41f8ce1fb1638438134b
Stupid7 <you@example.com>
2020-05-31 22:37:57 +0545

    styling left

--- js/main.js
+++ js/main.js
@@ -17,11 +17,16 @@ const addButton = document.getElementById("addButton"),
     endTime = document.getElementById("end_time"),
     itemSearchInput = document.getElementById("itemSearch");
 
-var shoppingRecordArr = [
-    { location: 'Oversea', items: 'copy', date: '03/12/1998', time: '10', cost: "44" },
-    { location: 'Toowoomba', items: 'car', date: '08/02/1875', time: '23', cost: "55" },
-    { location: 'Australia', items: 'Pen, paper', date: '31/12/1773', time: '01', cost: "33" },
-]
+var state = {
+    shoppingRecordArr: [
+        { location: 'Oversea', items: 'copy', date: '03/12/1998', time: '10', cost: "44" },
+        { location: 'Toowoomba', items: 'car', date: '08/02/1875', time: '23', cost: "55" },
+        { location: 'Toowoomba', items: 'car', date: '08/02/1875', time: '23', cost: "55" },
+        { location: 'Toowoomba', items: 'car', date: '08/02/1875', time: '23', cost: "55" },
+        { location: 'Australia', items: 'Pen, paper', date: '31/12/1773', time: '01', cost: "33" },
+    ],
+    summaryData: []
+}
 
 
 newCartForm.addEventListener('submit', addRecord);
@@ -29,51 +34,36 @@ searchForm.addEventListener('submit', searchRecord);
 
 function addRecord(e) {
     e.preventDefault();
-    let checkDate = validatedate(dateInput);
-    let checkTime = validatetime(timeInput);
-    if (checkDate && checkTime) {
-        let data = {};
-        for (let i = 0, length = radios.length; i < length; i++) {
-            if (radios[i].checked) {
-                data.location = radios[i].value;
-                break;
-            }
-        }
-        data.items = itemInput.value;
-        data.date = dateInput.value;
-        data.time = formatTime(timeInput.value);
-        data.cost = costInput.value;
-        shoppingRecordArr.push(data);
-        // newCartForm.reset();
-        showRecords();
-    }
+    controller.addRecord()
 }
 
 function validatedate(inputText) {
-    var opera1 = inputText.value.split('/');
-    if (opera1.length == 3) {
-        var dd = parseInt(opera1[0]);
-        var mm = parseInt(opera1[1]);
-        var yy = parseInt(opera1[2]);
+    let format = inputText.value.split('/');
+    let day, month, year;
+
+    if (format.length == 3) {
+        day = parseInt(format[0]);
+        month = parseInt(format[1]);
+        year = parseInt(format[2]);
     }
-    if (dd >= 1 && dd <= 31 && mm >= 1 && mm <= 12 && yy > 1583) {
-        var ListofDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        if (mm == 1 || mm > 2) {
-            if (dd > ListofDays[mm - 1]) {
+    if (day >= 1 && day <= 31 && month >= 1 && month <= 12 && year > 1583) {
+        let numberOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        if (month == 1 || month > 2) {
+            if (day > numberOfDays[month - 1]) {
                 alert('Invalid date format!');
                 return false;
             }
         }
-        if (mm == 2) {
-            var lyear = false;
-            if ((!(yy % 4) && yy % 100) || !(yy % 400)) {
-                lyear = true;
+        if (month == 2) {
+            let leapYear = false;
+            if ((!(year % 4) && year % 100) || !(year % 400)) {
+                leapYear = true;
             }
-            if ((lyear == false) && (dd >= 29)) {
+            if ((leapYear == false) && (day >= 29)) {
                 alert('Invalid date format!');
                 return false;
             }
-            if ((lyear == true) && (dd > 29)) {
+            if ((leapYear == true) && (day > 29)) {
                 alert('Invalid date format!');
                 return false;
             }
@@ -206,141 +196,6 @@ function IsNumeric(strString) {
     return blnResult;
 }
 
-function showRecords() {
-    let htmlContent = '';
-    for (let i = 0; i < shoppingRecordArr.length; i++) {
-        htmlContent += `
-        <tr>
-            <td>${shoppingRecordArr[i].location}</td>
-            <td>${shoppingRecordArr[i].date}</td>
-            <td>${shoppingRecordArr[i].time}</td>
-            <td>${shoppingRecordArr[i].items}</td>
-            <td>${shoppingRecordArr[i].cost}</td>
-        </tr>
-        `;
-    }
-    mainTableBody.innerHTML = '';
-    mainTableBody.innerHTML = htmlContent;
-}
-
-function tallyRecord() {
-    let summary = {};
-    let htmlContent = '';
-    for (let i = 0; i < shoppingRecordArr.length; i++) {
-        if (summary[shoppingRecordArr[i].location]) {
-            summary[shoppingRecordArr[i].location].no_of_record += 1;
-            summary[shoppingRecordArr[i].location].cost += Number(shoppingRecordArr[i].cost)
-        }
-        else {
-            summary[shoppingRecordArr[i].location] = {
-                location: shoppingRecordArr[i].location,
-                no_of_record: 1,
-                cost: Number(shoppingRecordArr[i].cost)
-            }
-        }
-    }
-
-    let summaryData = Object.values(summary);
-
-    for (let i = 0; i < summaryData.length; i++) {
-        htmlContent += `
-        <tr>
-            <td>${summaryData[i].location}</td>
-            <td>${summaryData[i].no_of_record}</td>
-            <td>${summaryData[i].cost}</td>
-        </tr>
-        `;
-    }
-
-    summaryBody.innerHTML = '';
-    summaryBody.innerHTML = htmlContent;
-}
-
-showRecords();
-
-tallyRecord();
-
-function sortByLocation() {
-    let newObject = {};
-    for (let i = 0; i < shoppingRecordArr.length; i++) {
-        if (newObject[shoppingRecordArr[i].location]) {
-            newObject[shoppingRecordArr[i].location].no_of_record += 1;
-        }
-        else {
-            newObject[shoppingRecordArr[i].location] = {
-                location: shoppingRecordArr[i].location,
-                no_of_record: 1,
-            }
-        }
-    }
-    let newArray = Object.values(newObject);
-    let length = newArray.length;
-    for (let i = 0; i < length; i++) {
-        for (let j = 0; j < (length - i - 1); j++) {
-            if (newArray[j].no_of_record < newArray[j + 1].no_of_record) {
-                let tmp = newArray[j];
-                newArray[j] = newArray[j + 1];
-                newArray[j + 1] = tmp;
-            }
-        }
-    }
-    let newShoppingArray = [];
-    for (let i = 0; i < newArray.length; i++) {
-        for (let j = 0; j < shoppingRecordArr.length; j++) {
-            if (shoppingRecordArr[j].location == newArray[i].location) {
-                newShoppingArray.push(shoppingRecordArr[j]);
-            }
-        }
-    }
-    console.log(newShoppingArray)
-
-    showRecords();
-}
-
-function sortByDate() {
-    let length = shoppingRecordArr.length;
-    for (let i = 0; i < length; i++) {
-        for (let j = 0; j < (length - i - 1); j++) {
-            let first = formatDate(shoppingRecordArr[j].date);
-            let second = formatDate(shoppingRecordArr[j + 1].date);
-            if (first > second) {
-                let tmp1 = shoppingRecordArr[j];
-                shoppingRecordArr[j] = shoppingRecordArr[j + 1];
-                shoppingRecordArr[j + 1] = tmp1;
-            }
-        }
-    }
-    showRecords();
-}
-
-function sortByCost() {
-    let length = shoppingRecordArr.length;
-    for (let i = 0; i < length; i++) {
-        for (let j = 0; j < (length - i - 1); j++) {
-            if (shoppingRecordArr[j].cost > shoppingRecordArr[j + 1].cost) {
-                let tmp = shoppingRecordArr[j];
-                shoppingRecordArr[j] = shoppingRecordArr[j + 1];
-                shoppingRecordArr[j + 1] = tmp;
-            }
-        }
-    }
-    showRecords();
-}
-
-function sortByItems() {
-    let length = shoppingRecordArr.length;
-    for (let i = 0; i < length; i++) {
-        for (let j = 0; j < (length - i - 1); j++) {
-            if (shoppingRecordArr[j].items > shoppingRecordArr[j + 1].items) {
-                let tmp = shoppingRecordArr[j];
-                shoppingRecordArr[j] = shoppingRecordArr[j + 1];
-                shoppingRecordArr[j + 1] = tmp;
-            }
-        }
-    }
-    showRecords();
-}
-
 function searchRecord(e) {
     e.preventDefault();
     let checkDate = validatedate(startDate) && validatedate(endDate);
@@ -357,6 +212,7 @@ function searchRecord(e) {
             console.log("start date and end date error")
         }
     }
+
     if (checkTime) {
         if (formatTime(startTime.value) <= formatTime(endTime.value)) {
             checkStartEndTime = true
@@ -365,23 +221,22 @@ function searchRecord(e) {
             console.log("start time and end time error")
         }
     }
-
     if (checkStartEndDate && checkStartEndTime) {
         let searchResultArray = [];
-        for (let i = 0; i < shoppingRecordArr.length; i++) {
-            let dateCondition = formatDate(shoppingRecordArr[i].date) >= formatDate(startDate.value)
-                && formatDate(shoppingRecordArr[i].date) <= formatDate(endDate.value);
-            let timeCondition = formatTime(shoppingRecordArr[i].time) >= formatTime(startTime.value)
-                && formatTime(shoppingRecordArr[i].time) <= formatTime(endTime.value);
+        for (let i = 0; i < state.shoppingRecordArr.length; i++) {
+            let dateCondition = formatDate(state.shoppingRecordArr[i].date) >= formatDate(startDate.value)
+                && formatDate(state.shoppingRecordArr[i].date) <= formatDate(endDate.value);
+            let timeCondition = Number(state.shoppingRecordArr[i].time) >= formatTime(startTime.value)
+                && Number(state.shoppingRecordArr[i].time) <= formatTime(endTime.value);
             let searchInputCondition = itemSearchInput.value !== ''
-                ? shoppingRecordArr[i].items.toLowerCase().includes(itemSearchInput.value.toLowerCase())
+                ? state.shoppingRecordArr[i].items.toLowerCase().includes(itemSearchInput.value.toLowerCase())
                 : true;
 
             if (dateCondition && timeCondition && searchInputCondition) {
-                searchResultArray.push({ ...shoppingRecordArr[i] })
+                searchResultArray.push({ ...state.shoppingRecordArr[i] })
             }
         }
-
+        console.log(searchResultArray)
         if (searchResultArray.length > 0) {
             let htmlContent = '';
             for (let i = 0; i < searchResultArray.length; i++) {
@@ -415,3 +270,173 @@ function formatDate(data) {
 function formatTime(data) {
     return Number(data.split(":")[0]);
 }
+
+var views = {
+    mainTableView(data) {
+        return `
+        <tr>
+            <td>${data.location}</td>
+            <td>${data.date}</td>
+            <td>${data.time}</td>
+            <td>${data.items}</td>
+            <td>${data.cost}</td>
+        </tr>
+        `;
+    },
+    summaryTableView(data) {
+        return `
+        <tr>
+            <td>${data.location}</td>
+            <td>${data.no_of_record}</td>
+            <td>${data.cost}</td>
+        </tr>
+        `;
+    }
+}
+
+var controller = {
+    addRecord() {
+        let checkDate = validatedate(dateInput);
+        let checkTime = validatetime(timeInput);
+        if (checkDate && checkTime) {
+            let data = {};
+            for (let i = 0, length = radios.length; i < length; i++) {
+                if (radios[i].checked) {
+                    data.location = radios[i].value;
+                    break;
+                }
+            }
+            data.items = itemInput.value;
+            data.date = dateInput.value;
+            data.time = formatTime(timeInput.value);
+            data.cost = costInput.value;
+            state.shoppingRecordArr.push(data);
+            // newCartForm.reset();
+            controller.showRecords();
+        }
+    },
+    showRecords() {
+        let htmlContent = '';
+        for (let i = 0; i < state.shoppingRecordArr.length; i++) {
+            htmlContent += views.mainTableView(state.shoppingRecordArr[i])
+        }
+        mainTableBody.innerHTML = '';
+        mainTableBody.innerHTML = htmlContent;
+    },
+    tallyRecord() {
+        let summary = {};
+        let htmlContent = '';
+        for (let i = 0; i < state.shoppingRecordArr.length; i++) {
+            if (summary[state.shoppingRecordArr[i].location]) {
+                summary[state.shoppingRecordArr[i].location].no_of_record += 1;
+                summary[state.shoppingRecordArr[i].location].cost += Number(state.shoppingRecordArr[i].cost)
+            }
+            else {
+                summary[state.shoppingRecordArr[i].location] = {
+                    location: state.shoppingRecordArr[i].location,
+                    no_of_record: 1,
+                    cost: Number(state.shoppingRecordArr[i].cost)
+                }
+            }
+        }
+
+        state.summaryData = Object.values(summary);
+
+        for (let i = 0; i < state.summaryData.length; i++) {
+            htmlContent += views.summaryTableView(state.summaryData[i])
+        }
+
+        summaryBody.innerHTML = '';
+        summaryBody.innerHTML = htmlContent;
+    },
+    sortByLocation() {
+        let newObject = {};
+        for (let i = 0; i < state.shoppingRecordArr.length; i++) {
+            if (newObject[state.shoppingRecordArr[i].location]) {
+                newObject[state.shoppingRecordArr[i].location].no_of_record += 1;
+            }
+            else {
+                newObject[state.shoppingRecordArr[i].location] = {
+                    location: state.shoppingRecordArr[i].location,
+                    no_of_record: 1,
+                }
+            }
+        }
+        let newArray = Object.values(newObject);
+        let length = newArray.length;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < (length - i - 1); j++) {
+                if (newArray[j].location > newArray[j + 1].location) {
+                    let tmp = newArray[j];
+                    newArray[j] = newArray[j + 1];
+                    newArray[j + 1] = tmp;
+                }
+            }
+        }
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < (length - i - 1); j++) {
+                if (newArray[j].no_of_record < newArray[j + 1].no_of_record) {
+                    let tmp = newArray[j];
+                    newArray[j] = newArray[j + 1];
+                    newArray[j + 1] = tmp;
+                }
+            }
+        }
+        let newShoppingArray = [];
+        for (let i = 0; i < newArray.length; i++) {
+            for (let j = 0; j < state.shoppingRecordArr.length; j++) {
+                if (state.shoppingRecordArr[j].location == newArray[i].location) {
+                    newShoppingArray.push(state.shoppingRecordArr[j]);
+                }
+            }
+        }
+        state.shoppingRecordArr = [...newShoppingArray];
+        controller.showRecords();
+    },
+    sortByDate() {
+        let length = state.shoppingRecordArr.length;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < (length - i - 1); j++) {
+                let first = formatDate(state.shoppingRecordArr[j].date);
+                let second = formatDate(state.shoppingRecordArr[j + 1].date);
+                if (first > second) {
+                    controller.swap(j);
+                }
+            }
+        }
+        controller.showRecords();
+    },
+    sortByCost() {
+        let length = state.shoppingRecordArr.length;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < (length - i - 1); j++) {
+                if (state.shoppingRecordArr[j].cost > state.shoppingRecordArr[j + 1].cost) {
+                    controller.swap(j);
+                }
+            }
+        }
+        controller.showRecords();
+    },
+    sortByItems() {
+        let length = state.shoppingRecordArr.length;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < (length - i - 1); j++) {
+                if (state.shoppingRecordArr[j].items > state.shoppingRecordArr[j + 1].items) {
+                    controller.swap(j);
+                }
+            }
+        }
+        controller.showRecords();
+    },
+    swap(j) {
+        let tmp = state.shoppingRecordArr[j];
+        state.shoppingRecordArr[j] = state.shoppingRecordArr[j + 1];
+        state.shoppingRecordArr[j + 1] = tmp;
+    },
+    onInit() {
+        controller.showRecords();
+        controller.tallyRecord();
+    }
+}
+
+controller.onInit();
